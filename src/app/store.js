25b716f761415d18d4de805605437728a0bc03d7
Chris <stylesuxx@gmail.com>
2022-09-13 20:31:32 +0200

    Feature/healthchecks (#124)
    * Add health checks and an automatism to automatically fix known issues
    * Add a donation banner before the rooting step

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import aboutReducer from "../features/about/aboutSlice";
 import deviceReducer from "../features/device/deviceSlice";
+import donateReducer from "../features/donate/donateSlice";
 import healthcheckReducer from "../features/healthcheck/healthcheckSlice";
 import packagesReducer from "../features/packages/packagesSlice";
 import rootReducer from "../features/root/rootSlice";
@@ -11,6 +12,7 @@ export const store = configureStore({
   reducer: {
     about: aboutReducer,
     device: deviceReducer,
+    donate: donateReducer,
     healthcheck: healthcheckReducer,
     packages: packagesReducer,
     root: rootReducer,
