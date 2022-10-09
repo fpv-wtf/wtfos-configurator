9287ec4840d00a9461c78a564b92748df20b7925
Chris <stylesuxx@gmail.com>
2022-10-09 22:07:14 +0200

    Refactored package parser into seperate file (#198)

--- src/utils/AdbWrapper.js
+++ src/utils/AdbWrapper.js
@@ -9,6 +9,7 @@ import busybox from "./busybox";
 
 import Proxy from "./Proxy";
 import ReverseShellSocket from "./ReverseShellSocket";
+import { parsePackageIndex } from "./OpkgHelpers";
 
 const proxy = new Proxy("https://cors.bubblesort.me/?");
 
@@ -203,27 +204,8 @@ export default class AdbWrapper {
       "gunzip -c",
       `${this.wtfos.opkgLists}/${repo}`,
     ]);
-    const lines = output.stdout.split("\n");
 
-    let currentPackage = null;
-    const packages = {};
-    for(let i = 0; i < lines.length; i += 1) {
-      const line = lines[i];
-      const fields = line.split(": ");
-      let key = fields.shift();
-      const value = fields.join(": ");
-
-      if(key === "Package") {
-        packages[value] = {};
-        currentPackage = value;
-      } else {
-        key = key.toLowerCase();
-
-        packages[currentPackage][key] = value;
-      }
-    }
-
-    return packages;
+    return parsePackageIndex(output.stdout);
   }
 
   async getPackageDetails(name) {
