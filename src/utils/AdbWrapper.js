76c8c4d35f7261ba80af87bc8d7f4a74ede3e090
Chris L <stylesuxx@gmail.com>
2022-10-30 14:09:51 +0100

    Run adb removal (if present) during wtfOS removal. Resolves #214

--- src/utils/AdbWrapper.js
+++ src/utils/AdbWrapper.js
@@ -25,6 +25,7 @@ export default class AdbWrapper {
         dinit: "/opt/sbin/dinit",
         dinitctl: "/opt/sbin/dinitctl",
         opkg: "/opt/bin/opkg",
+        adbRemoval: "/system/bin/wtfos-remove-adb",
       },
       config: {
         dinit: "/opt/etc/dinit.d",
@@ -772,6 +773,19 @@ export default class AdbWrapper {
       statusCallback("ERROR: Failed cleaning up");
       output.stdout.split("\n").forEach((line) => statusCallback(line));
       return;
+    }
+
+    const hasAdbRemoval = await this.fileExists(this.wtfos.bin.adbRemoval);
+    if(hasAdbRemoval) {
+      statusCallback("Removing ADB...");
+      output = await this.executeCommand([
+        this.wtfos.bin.adbRemoval,
+        "-q",
+      ]);
+      if(output.exitCode !== 0) {
+        statusCallback("ERROR: Failed removing ADB");
+        return;
+      }
     }
 
     statusCallback("Rebooting...");
